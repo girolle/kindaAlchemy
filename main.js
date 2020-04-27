d17b9b994529d9274886bf9b978bf8f1a64c6607
Girolle <khas98@gmail.com>
2020-04-27 06:34:01 +0300

    well

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
 var $draggables;
 
-$(document).ready( function() {
+$(document).ready(function () {
   $draggables = $('.draggable').draggabilly({
-    containment: true,
-    grid: [ 2, 2 ], 
+    grid: [2, 2],
   });
-
-  
 });
-var b = [];
-b.push($('#0'))
-b.push($("#1"));
-b.push($("#2"));
-b.push($("#3"));
 
-for (let i = 0; i < 4; i+=1) {
-  b[i].on('dragEnd', function(){
-  console.log(b[i][0].style.top + '    ' + b[i][0].style.left);
 
-});
+
+
+var c = [];
+var m = [];
+for (let i = 0; i < 3; i++){
+  c.push([]);
+  for (let j = 0; j < 4; j++) {
+    c[i].push($('#c'+i+'_'+j));
+    c[i][j].draggabilly({ containment: ("#ch" + i) });
+    c[i][j].on('dragEnd', function () {
+      if ((parseInt(c[i][j][0].style.top) + scale) > contHeight) {
+        m[i] = j;
+        console.log(m);
+
+        for (let k = 0; k < 4; k++) {
+          if (k!=j) {
+            c[i][k].draggabilly({ containment: ('#c'+ i) });
+          }
+        }
+      }
+      if (m[i]==j && ((parseInt(c[i][j][0].style.top) + scale) < contHeight)) {
+        m[i] = undefined;
+        for (let k = 0; k < 4; k++) {
+          c[i][k].draggabilly({containment: ("#ch" + i) });
+        }
+      }
+      if (m[0]!=undefined && m[1]!=undefined && m[2]!= undefined) {
+        for (let k = 0; k < 3; k++) {
+          c[k][m[k]].animate({
+            top: (contHeight * 1.5 - scale / 2) + "px",
+            left: ((windowWidth * 0.9 - scale) / 2) + "px"
+          });
+        }
+      }
+    });
+  }
 }
+
--- scale_init.js
+++ scale_init.js
@@ -1,16 +1,20 @@
 windowHeight = 0.99 * document.documentElement.clientHeight;
 windowWidth = 0.99 * document.documentElement.clientWidth;
-contHeight = windowHeight * 0.5;
+contHeight = windowHeight * 0.45;
 contWidth = windowWidth * 0.9 * 0.3;
-let scale = (Math.min((Math.max(contHeight, contWidth))/4, Math.min(contHeight, contWidth))) * 0.9 ;
+let scale = (Math.min((Math.max(contHeight, contWidth))/4, Math.min(contHeight, contWidth))) * 0.99 ;
 console.log(contHeight + "   " + contWidth)
 
 for (let i = 0; i<3; i+=1) {
 	for (let j = 0; j < 4; j+=1) {
 		document.getElementById("c"+i+"_"+j).style.width = scale + "px";
 		document.getElementById("c"+i+"_"+j).style.height = scale + "px";
-
 	}
 }
 
-containers = document.getElementsByClassName("container")
+containers = document.getElementsByClassName("container");
+for (let i = 0; i<3; i+=1) {
+	for (let j = 0; j < 4; j+=1) {
+		document.getElementById("c"+i+"_"+j).style.left = ($("#ch"+i).position().left + (windowWidth * 0.9 * 0.1) / 6) + "px";
+	}
+}
