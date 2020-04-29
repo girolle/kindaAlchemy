0270e5dec2efcbf9ec8115b4e543f30c23b16a3a
Girolle <khas98@gmail.com>
2020-04-29 13:38:17 +0300

    sorry

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ for (let i = 0; i < 3; i++){
   c.push([]);
   for (let j = 0; j < 4; j++) {
     c[i].push($('#c'+i+'_'+j));
+    if (i == 0) {
+      c[i][j][0].style.backgroundColor = "red";
+    }
+    if (i == 2) {
+      c[i][j][0].style.backgroundColor = "green";
+    }
     c[i][j].draggabilly({ containment: ("#ch" + i) });
     c[i][j].on('dragEnd', function () {
       if ((parseInt(c[i][j][0].style.top) + scale) > contHeight) {
@@ -35,10 +41,25 @@ for (let i = 0; i < 3; i++){
       }
       if (m[0]!=undefined && m[1]!=undefined && m[2]!= undefined) {
         for (let k = 0; k < 3; k++) {
-          c[k][m[k]].animate({
-            top: (contHeight * 1.5 - scale / 2) + "px",
-            left: (($("#containers").width() - scale) / 2) + "px"
+            c[k][m[k]].animate({
+            top: (contHeight * 1.5 + scale) + "px",
+            left: (($("#containers").width() + scale) / 2) + "px"
           });
+          c[k][m[k]][0].style.transform = "translate(-100%, -100%)";
+          
+          if (k == 0) {
+            c[k][m[k]][0].style.transform = "translate(-83%, -100%)"
+            c[k][m[k]].animate({
+              width :  (parseInt(c[k][m[k]][0].style.width) * 1.5 ) + "px", 
+              height: (parseInt(c[k][m[k]][0].style.height) * 1.5 ) + "px"
+          });
+        }
+          if (k == 2) {
+            c[k][m[k]].animate({
+              width :  (parseInt(c[k][m[k]][0].style.width) * 0.5 ) + "px", 
+              height: (parseInt(c[k][m[k]][0].style.height) * 0.5 ) + "px"
+          });
+          }
         }
       }
     });
